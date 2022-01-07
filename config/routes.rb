ffd3d0ae5ce7c5c966afb2ed8d4bda0da2343b67
Babin Bohora <bohorababin1@gmail.com>
2022-01-07 15:51:49 +0900

    controller generated and index page done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Create, Read, Update, Delete
+  # read all the Restaurants
+  # get '/restaurants', to: 'restaurants#index'
+  # # read one Restaurant
+  # get '/restaurants/:id', to: 'restaurants#show'
+  # # Create a Restaurant
+  # get '/restaurants/new', to: 'restaurants#new'
+  # post '/restaurants', to: 'restaurants#create'
+  # # Update a Restaurant
+  # get '/restaurants/:id/edit', to: 'restaurants#edit'
+  # patch '/restaurants/:id', to: 'restaurants#update'
+  # # delete a Restaurant
+  # delete '/restaurants/:id', to: 'restaurants#destroy'
+
+  resources :restaurants
 end
